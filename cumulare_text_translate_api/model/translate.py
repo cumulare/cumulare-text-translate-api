56a851a7e3fa291f5c599853a7a41bfea1a123ac
Spacious <spacious@Alissas-MBP.lan>
2023-10-15 14:47:53 -0400

    Fix more errors

--- cumulare_text_translate_api/model/translate.py
+++ cumulare_text_translate_api/model/translate.py
@@ -1,5 +1,13 @@
+
 class Translate:
+    """Used for translating images
+    """    
 
     def dict(self) -> None:
+        """
+        Used for going through dic of images.
+
+        Returns success if all have been processed
+        """
         return None
 
